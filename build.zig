95b7f930abeedc639f3e8d1f7c78084c7ef4e331
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2024-06-15 21:57:25 +0200

    upgrade to zig 0.13.0

--- build.zig
+++ build.zig
@@ -10,27 +10,27 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    libsecp256k1.addIncludePath(.{ .path = "libsecp256k1" });
-    libsecp256k1.addIncludePath(.{ .path = "libsecp256k1/src" });
+    libsecp256k1.addIncludePath(b.path("libsecp256k1"));
+    libsecp256k1.addIncludePath(b.path("libsecp256k1/src"));
     libsecp256k1.defineCMacro("USE_FIELD_10X26", "1");
     libsecp256k1.defineCMacro("USE_SCALAR_8X32", "1");
     libsecp256k1.defineCMacro("USE_ENDOMORPHISM", "1");
     libsecp256k1.defineCMacro("USE_NUM_NONE", "1");
     libsecp256k1.defineCMacro("USE_FIELD_INV_BUILTIN", "1");
     libsecp256k1.defineCMacro("USE_SCALAR_INV_BUILTIN", "1");
-    libsecp256k1.addCSourceFile(.{ .file = .{ .path = "ext.c" }, .flags = &[0][]const u8{} });
+    libsecp256k1.addCSourceFile(.{ .file = b.path("ext.c"), .flags = &[0][]const u8{} });
     libsecp256k1.linkLibC();
     b.installArtifact(libsecp256k1);
 
     // Run command.
     const exe = b.addExecutable(.{
         .name = "zig-eth-secp256k1",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    exe.addIncludePath(.{ .path = "." });
-    exe.addIncludePath(.{ .path = "libsecp256k1" });
+    exe.addIncludePath(b.path("."));
+    exe.addIncludePath(b.path("libsecp256k1"));
     exe.linkLibrary(libsecp256k1);
 
     const run_cmd = b.addRunArtifact(exe);
@@ -45,12 +45,12 @@ pub fn build(b: *std.Build) void {
 
     // Tests.
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    main_tests.addIncludePath(.{ .path = "." });
-    main_tests.addIncludePath(.{ .path = "libsecp256k1" });
+    main_tests.addIncludePath(b.path("."));
+    main_tests.addIncludePath(b.path("libsecp256k1"));
     main_tests.linkLibrary(libsecp256k1);
 
     const run_main_tests = b.addRunArtifact(main_tests);
@@ -59,5 +59,5 @@ pub fn build(b: *std.Build) void {
     test_step.dependOn(&run_main_tests.step);
 
     // Exported modules.
-    _ = b.addModule("zig-eth-secp256k1", .{ .source_file = .{ .path = "src/secp256k1.zig" } });
+    _ = b.addModule("zig-eth-secp256k1", .{ .root_source_file = b.path("src/secp256k1.zig") });
 }
